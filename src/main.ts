fb9f7a0c197c1e3e67a5929bf538bac77ae55433
Amir-Mallek <amir.mallek@insat.ucar.tn>
2024-11-06 22:29:40 +0100

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow specific HTTP methods
+    credentials: true, // Allow cookies and credentials if needed
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(3033);
 }
